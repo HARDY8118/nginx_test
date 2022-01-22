fc2aac5aef000e17380df014e31c7da5fda6d016
Harshit Vishnoi <5825564-HARDY8118@users.noreply.gitlab.com>
2022-01-22 20:09:52 +0530

    Updated watch script

--- watch.sh
+++ watch.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-nodemon -e conf -x "./down.sh && ./up.sh && ./logs.sh"
+nodemon --help
+if [[ $? -eq 0 ]]
+then
+    clear;
+    nodemon -e conf -x "./down.sh && ./up.sh && ./logs.sh";
+else
+    echo "Nodemon not found";
+    echo 'Install nodemon with "yarn global add nodemon" or "npm install -g nodemon"';
+fi
