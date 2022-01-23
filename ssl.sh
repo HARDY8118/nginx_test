c54c5b35fd57a03b54a238363d4114d70b1412fe
Harshit Vishnoi <5825564-HARDY8118@users.noreply.gitlab.com>
2022-01-23 16:46:15 +0530

    Added function to add diffie hellman

--- ssl.sh
+++ ssl.sh
@@ -6,9 +6,16 @@ export file_name=self
 
 function generateSSLCertificate {
     echo "Creating a self signed certificate of ${key_size} bits valid for ${days} days";
-    openssl req -x509 -days ${days} -nodes -newkey rsa:${key_size} -keyout ${PWD}/nginx/ssl/${file_name}.key -out ${PWD}/nginx/ssl/${file_name}.crt
+    openssl req -x509 -days ${days} -nodes -newkey rsa:${key_size} -keyout ${PWD}/nginx/ssl/${file_name}.key -out ${PWD}/nginx/ssl/${file_name}.crt;
+}
+
+function generateDHParam {
+    echo "Creating Diffie Hellman parameters of ${key_size} bits";
+    openssl dhparam -out ${PWD}/nginx/ssl/dhparam.pem ${key_size};
 }
 
 if [[ $1 == "ssl" ]]; then
     generateSSLCertificate
+elif [[ $1 == "dh" ]]; then
+    generateDHParam
 fi
